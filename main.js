f24b71b165e17685458b79f682bdc6bf8a2c7148
wlgus95799@naver.com <wlgus95799@naver.com>
2024-07-11 17:01:58 +0900

    client js실습

---
+++ main.js
@@ -0,0 +1,13 @@
+// 1.alert() : 알림창을 호출하는 메세지를 출력하는 명령
+// 2.console.log() : (브라우저나 터미널에) 콘솔에 메세지를 출력하는 명령
+// 코드를 실행할때 오류가 생기거나 예기치 않을 결과가 나올떄 ==>디버그
+// 3.document.write() :문서의 body 영역에 메세지/HTML을 출력하는 명령
+
+//alert(10);
+// alert("Hello js!");
+// alert(10+5);
+
+// document.write 는 숫자,문자 데이터 vs
+document.write ("10");
+document.write ("<h1>Hello, js!</h1>");
+document.write ("<a href='http://www.naver.com'>네이버</a>");
